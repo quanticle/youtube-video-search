19940b1d07368421db02ec797b7ffad9b0c808fb
Rohit Patnaik <quanticle@quanticle.net>
2023-06-15 08:02:53 -0500

    Changed license to GPL3

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject youtube-video-search "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "A small app to search for videos using the YouTube API"
+  :url "https://github.com/quanticle/youtube-video-search"
+  :license {:name "GPL 3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot youtube-video-search.core
   :target-path "target/%s"
